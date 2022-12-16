3ace92693ef94fc76a5466d802f0b0596ccc3c5e
harkinder-dark <victorvaddely@gmail.com>
2022-12-16 10:00:11 +0000

    correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,38 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure
- *
- * Return: If tree is NULL, must return 0
- */
-int binary_tree_balance(const binary_tree_t *tree)
+ * binary_tree_height - height
+ * @tree: node
+ * Return: unsigned int
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-	}
-	return (0);
+size_t n = 0, r = 0;
+if (tree == NULL)
+return (0);
+
+n = (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
+r = (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
+return ((r > n) ? r : n);
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: if the tree is NULL, must return 0
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
+ * binary_tree_balance - balance
+ * @tree: node
+ * Return: int
+*/
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t l = 0, r = 0;
+if (tree == NULL)
+return (0);
 
-	if (tree)
-	{
-		l = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
-
-		r = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
-		return (l > r ? l : r);
-	}
-	return (0);
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,19 +2,18 @@
 
 /**
  * binary_tree_full - node full
- * Description - ...
  * @tree: node
  * Return: int
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (1);
+if (tree != NULL)
+{
 if ((tree->left != NULL && tree->right == NULL) ||
 (tree->left == NULL && tree->right != NULL) ||
 binary_tree_is_full(tree->left) == 0 ||
 binary_tree_is_full(tree->right) == 0)
 return (0);
-
+}
 return (1);
 }
